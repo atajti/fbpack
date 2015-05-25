0207cffae9af0a9bf26546ff15c8687b25d1034f
atajti <atajti@gmail.com>
2015-05-26 01:33:14 +0200

    added limit for waiting user login (5 sec... should it be tunable?), and browser close in case of log out error.

--- R/search_fb.R
+++ R/search_fb.R
@@ -13,8 +13,9 @@ search_fb <- function(x, login_name, login_password,
   # Function to find out wether the user logged in already
   #
   wait_for_user_login <- function(remDr){
+    startime <- Sys.time()
     login_done <- list()
-    while(length(login_done)==0){
+    while(length(login_done)==0 | Sys.time()-startime < 5){
         tryCatch({login_done <- remDr$findElement(using = 'css selector',
                    "div._4r_y")},
             error = function(e){NULL},
@@ -207,15 +208,21 @@ search_fb <- function(x, login_name, login_password,
     #}
   # Log out, close browser
   #
+  tryCatch({
+    log_out_element <- remDr$findElement(using = "id",
+                         value = "pageLoginAnchor")
+    log_out_element$clickElement()
 
-  log_out_element <- remDr$findElement(using = "id",
-                       value = "pageLoginAnchor")
-  log_out_element$clickElement()
-
-  log_out_form <- remDr$findElements(using = "class name",
-                       value = "_54nc")
-  log_out_form <- log_out_form[[length(log_out_form)-2]]
-  log_out_form$clickElement()
+    log_out_form <- remDr$findElements(using = "class name",
+                         value = "_54nc")
+    log_out_form <- log_out_form[[length(log_out_form)-2]]
+    log_out_form$clickElement()},
+    error=function(e){warning(paste0("Error:\n", e))
+                      return(found_entities[1:number_of_results])
+                      remDr$close()},
+    warning=function(w){warning(paste0("Warning:\n", w))
+                      return(found_entities[1:number_of_results])
+                      remDr$close()})
   # close remote_driver, write data, clear everything and close R
   remDr$close()
 
